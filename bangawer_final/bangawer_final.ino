d9ead32c879dc3b6b487c5bf67b3cdf28f20130a
kangbba <121343882+kangbba@users.noreply.github.com>
2023-07-31 21:47:08 +0900

    tested

--- bangawer_final/bangawer_final.ino
+++ bangawer_final/bangawer_final.ino
@@ -68,12 +68,12 @@ int gapWithTextLines = 24;
 
 //데이터 전송속도 옵션
 #define CHUNK_SIZE 240
-#define CHUNK_DELAY 15
+#define CHUNK_DELAY 10
 
 //용량 옵션   
 #define RECORDING_TIME 5
 #define SAMPLE_RATE 6000
-#define MICROSECOND_DELAY 36
+#define MICROSECOND_DELAY 60
 //실제 녹음시간이 RECORDING_TIME보다 많이나오면 MICROSECOND_DELAY를 줄여야함
 
 
@@ -113,10 +113,12 @@ void setup()
 class MyServerCallbacks: public BLEServerCallbacks {
   void onConnect(BLEServer* pServer) {
     deviceConnected = true;
+    centerText("CONNECTED");
   };
 
   void onDisconnect(BLEServer* pServer) {
     deviceConnected = false;
+    centerText("DISCONNECTED");
   }
 };
 class MyRXCallbacks: public BLECharacteristicCallbacks {
@@ -149,6 +151,8 @@ class MyRXCallbacks: public BLECharacteristicCallbacks {
         recentMessage = msg;
         newRecentMsgExist = true;
         isRecentMsgGood = msg.length() > 0 && msg.indexOf(":") != -1 && msg.indexOf(";") != -1;
+        Serial.print("새 메세지 도착 : ");
+        Serial.println(newRecentMsgExist);
       }
 
       // uint8_t endPattern[] = {0x4F, 0x4B}; // 'O'와 'K'의 ASCII 코드
@@ -540,59 +544,6 @@ void initRecording(){
   memset(buffer, 0, RECORDING_DATA_SIZE);
 }
 
-void CreateWavHeader(byte *header, int waveDataSize)
-{
-  header[0] = 'R';
-  header[1] = 'I';
-  header[2] = 'F';
-  header[3] = 'F';
-  unsigned int fileSizeMinus8 = waveDataSize + 44 - 8;
-  header[4] = (byte)(fileSizeMinus8 & 0xFF);
-  header[5] = (byte)((fileSizeMinus8 >> 8) & 0xFF);
-  header[6] = (byte)((fileSizeMinus8 >> 16) & 0xFF);
-  header[7] = (byte)((fileSizeMinus8 >> 24) & 0xFF);
-  header[8] = 'W';
-  header[9] = 'A';
-  header[10] = 'V';
-  header[11] = 'E';
-  header[12] = 'f';
-  header[13] = 'm';
-  header[14] = 't';
-  header[15] = ' ';
-  header[16] = 0x10;  // linear PCM
-  header[17] = 0x00;
-  header[18] = 0x00;
-  header[19] = 0x00;
-  header[20] = 0x01;  // linear PCM
-  header[21] = 0x00;
-  header[22] = NUM_CHANNELS;  // monoral
-  header[23] = 0x00;
-  int sampleRateValue = SAMPLE_RATE;
-  header[24] = sampleRateValue & 0xFF;
-  header[25] = (sampleRateValue >> 8) & 0xFF;
-  header[26] = (sampleRateValue >> 16) & 0xFF;
-  header[27] = (sampleRateValue >> 24) & 0xFF;
-  int byteRate = SAMPLE_RATE * NUM_CHANNELS * SAMPLE_SIZE;
-  header[28] = byteRate & 0xFF;        // Byte/sec
-  header[29] = (byteRate >> 8) & 0xFF;
-  header[30] = (byteRate >> 16) & 0xFF;
-  header[31] = (byteRate >> 24) & 0xFF;
-  header[32] = NUM_CHANNELS * SAMPLE_SIZE;  // Block align
-  header[33] = 0x00;
-  header[34] = 8 * SAMPLE_SIZE;  // Bits per sample
-
-  header[35] = 'd';
-  header[36] = 'a';
-  header[37] = 't';
-  header[38] = 'a';
-  header[39] = (byte)(waveDataSize & 0xFF);
-  header[40] = (byte)((waveDataSize >> 8) & 0xFF);
-  header[41] = (byte)((waveDataSize >> 16) & 0xFF);
-  header[42] = (byte)((waveDataSize >> 24) & 0xFF);
-}
-
-
-
 //format bytes
 String formatBytes(size_t bytes)
 {
@@ -657,8 +608,8 @@ void loop()
     Serial.println("PRE_RECORDING");
     write_data_count = 0;
     strcpy(filename, "/sound1.wav");
-    recordStartMilis = millis();// LED ON)
     delay(600);
+    recordStartMilis = millis();// LED ON)
     recordMode = RECORD_MODE_RECORDING;
     centerText("RECORDING");
     sendMsgToFlutter("");
@@ -701,7 +652,6 @@ void loop()
       recordMode = RECORD_MODE_READY;
       return;
     }
-    CreateWavHeader(header, RECORDING_DATA_SIZE);
 
     Serial.println(headerSize);
     int sum_size = 0;
